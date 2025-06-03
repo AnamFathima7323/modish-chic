27056d4a3ae3501eaca035741e4131a45a3a4130
AnamFathima7323 <36875879+AnamFathima7323@users.noreply.github.com>
2025-06-03 15:13:18 +0400

    Add Firebase configuration and setup

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+ECHO is on.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1hVigeReA9eBWm20yEJ-BTPIU2-xOpV8",
+  authDomain: "modish-chic-af711.firebaseapp.com",
+  projectId: "modish-chic-af711",
+  storageBucket: "modish-chic-af711.firebasestorage.app",
+  messagingSenderId: "731851682356",
+  appId: "1:731851682356:web:cfd715d42e9f8f94d55061"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
